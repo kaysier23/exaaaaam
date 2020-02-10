2abc9fd91740baaf43fad7410b680711e80dce22
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-10 23:33:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,46 @@
  $content = $domOBJ->getElementsByTagName("item");
  
  ?>
-  <h1>Latest Science News -- ScienceDaily</h1>
+
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="index.php">science news</a></li>
+  <li><a href="tech.php">technology news</a></li>
+</ul>
+
+</body>
+</html>
+
+<h1>Latest Science News -- ScienceDaily</h1>
 
     <?php
  foreach( $content as $data )
