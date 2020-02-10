e34e4a277765ff8834c20c355dfbc429c1be32b9
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-10 23:26:24 +0800

    Update rss.php

--- rss.php
+++ rss.php
@@ -29,6 +29,42 @@
 			<pubDate>Thu, 06 Feb 2020 14:48:37 EST</pubDate>
 			<guid isPermaLink="true">https://www.sciencedaily.com/releases/2020/02/200206144837.htm</guid>
 		</item>
+
+		<title>Top Technology News -- ScienceDaily</title>
+		<link>https://www.sciencedaily.com/news/top/technology/</link>
+		<description>Top stories featured on ScienceDaily's Space &amp; Time, Matter &amp; Energy, and Computers &amp; Math sections.</description>
+		<language>en-us</language>
+		<pubDate>Mon, 10 Feb 2020 09:53:03 EST</pubDate>
+		<lastBuildDate>Mon, 10 Feb 2020 09:53:03 EST</lastBuildDate>
+		<ttl>60</ttl>
+		<image>
+			<title>Top Technology News -- ScienceDaily</title>
+			<url>https://www.sciencedaily.com/images/scidaily-logo-rss.png</url>
+			<link>https://www.sciencedaily.com/news/top/technology/</link>
+			<description>For more science news, visit ScienceDaily.</description>
+		</image>
+		<atom:link xmlns:atom="http://www.w3.org/2005/Atom" rel="self" href="https://www.sciencedaily.com/rss/top/technology.xml" type="application/rss+xml" />
+		<item>
+			<title>First-of-its-kind hydrogel platform enables on-demand production of medicines, chemicals</title>
+			<link>https://www.sciencedaily.com/releases/2020/02/200209155505.htm</link>
+			<description>A team of chemical engineers has developed a new way to produce medicines and chemicals on demand and preserve them using portable &#039;&#039;biofactories&#039;&#039; embedded in water-based gels called hydrogels. The approach could help people in remote villages or on military missions, where the absence of pharmacies, doctor&#039;s offices or even basic refrigeration makes it hard to access critical medicines, daily use chemicals and other small-molecule compounds. <!-- more --></description>
+			<pubDate>Sun, 09 Feb 2020 15:55:05 EST</pubDate>
+			<guid isPermaLink="true">https://www.sciencedaily.com/releases/2020/02/200209155505.htm</guid>
+		</item>
+		<item>
+			<title>Bioengineers 3D-print implants to seed multiple layers of tissue</title>
+			<link>https://www.sciencedaily.com/releases/2020/02/200208094311.htm</link>
+			<description>Bioengineers print 3D implants with layered cells destined to become distinct combinations of tissue, like bone and cartilage. The scaffolds degrade over time to leave the natural tissues in place. <!-- more --></description>
+			<pubDate>Sat, 08 Feb 2020 09:43:11 EST</pubDate>
+			<guid isPermaLink="true">https://www.sciencedaily.com/releases/2020/02/200208094311.htm</guid>
+		</item>
+		<item>
+			<title>Novel techniques for mining patented gene therapies offer promising treatment options</title>
+			<link>https://www.sciencedaily.com/releases/2020/02/200207141703.htm</link>
+			<description>Scientists are working to gain a better understand of the growing number of worldwide patented innovations available for gene therapy treatment. <!-- more --></description>
+			<pubDate>Fri, 07 Feb 2020 14:17:03 EST</pubDate>
+			<guid isPermaLink="true">https://www.sciencedaily.com/releases/2020/02/200207141703.htm</guid>
+		</item>
 	</channel>
 </rss>
 <!-- Page successfully written to cache. -->
