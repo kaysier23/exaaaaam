3e5402902c9ec38f3c016c007f3ac82966a03e26
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-02-10 23:38:05 +0800

    Update tech.php

--- tech.php
+++ tech.php
@@ -5,6 +5,45 @@
  $content = $domOBJ->getElementsByTagName("item");
  
  ?>
+<html>
+<head>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="index.php">science news</a></li>
+  <li><a href="tech.php">technology news</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
+
+</body>
+</html>
   <h1>TOP NEWS FOR TECHNOLOGY</h1>
 
     <?php
